0ad01fa01c0a375ad7b50a95095e2da0fbff2858
hosi153 <ho11ho11ho11@naver.com>
2023-10-04 03:11:59 +0000

    Delete src directory

--- src/main/java/com/example/burgerqueen_proj/product/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.burgerqueen_proj.product.repository;
-
-import com.example.burgerqueen_proj.product.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/com/example/burgerqueen_proj/product/repository/PromotionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.burgerqueen_proj.product.repository;
-
-import com.example.burgerqueen_proj.product.entity.Promotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-}
